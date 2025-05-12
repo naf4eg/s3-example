47fca94a0b87a144e7d19188b3fa209e8bb9b46e
Konstantin Bachev <naf4eg@yandex.ru>
2025-05-13 01:56:57 +0300

    added docker

--- README.md
+++ README.md
@@ -3,5 +3,13 @@ This is Amazon S3 client example with MINIO and Spring Boot.
 For example put image.jpg file and JasperReport report.pdf file.
 
 # Getting Started
-After start container run [MINIO UI](http://127.0.0.1:9001)
+
+1) Create dir:
+```bash
+mkdir -p ~/minio/data
+```
+2) Run compose.yaml
+
+
+3) After start container run [MINIO UI](http://127.0.0.1:9001)
 
--- src/main/java/com/bachev/s3/example/s3_example/S3ExampleApplication.java
+++ src/main/java/com/bachev/s3/example/s3_example/S3ExampleApplication.java
@@ -5,15 +5,19 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.ResourceUtils;
 
+import javax.sql.DataSource;
 import java.io.FileNotFoundException;
 
 @SpringBootApplication
 @Slf4j
+@RequiredArgsConstructor
 public class S3ExampleApplication {
 
 	public static void main(String[] args) throws Exception {
